e1ecbcadda54f28e96dcd541aa2dd6f844bea49f
Alexei Rusu <rusualexei1995@gmail.com>
2023-03-11 14:24:53 -0500

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import Layout from "@/components/Layout";
+import NavBar from "@/components/NavBar";
+import "../styles/globals.css"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}){
+
+    return (<>
+       <Layout>
+            <Component {...pageProps} />
+                  
+       </Layout>
+
+
+    </>
+    )
 }
+
+
+
+{/* you can add the global styles here as well */}  
+{/* <style jsx global>{`
+nav {
+ background-color: tomato;
+}
+a {
+ text-decoration: none !important;
+ color: green;
+}
+.active {
+ color: white;
+}
+`}</style> */}
